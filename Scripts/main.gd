e847a1b083f06e1c708f871d46d87598132e516f
Reegan Mitchell <45948202+ReeganMitchell@users.noreply.github.com>
2021-02-15 17:59:29 +0000

    Menu and first Export
    First web export test to host on github

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,13 +3,34 @@ extends Node
 
 # Declare member variables here
 var state = 0 #0 = menu, 1 = how to, 2 = game
-
+onready var animplayer = $MenuAnimPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	state = 0
+	animplayer.play_backwards("StartGame")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_StartButton_pressed():
+	if (state == 0 ):
+		animplayer.play("StartGame")
+		state = 2
+
+
+
+func _on_How2Button_pressed():
+	if (state == 0):
+		animplayer.play("HowToPlay")
+		state = 1
+
+
+
+func _on_Button_pressed():
+	if (state == 1):
+		animplayer.play_backwards("HowToPlay")
+		state = 0
